9d4de1366c1ccfbddf2db1fbf110ee485af205c2
Indra Tanaya <aryaindratanaya@gmail.com>
2022-09-17 02:13:12 +0800

    Create a dedicated page to display a list of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
